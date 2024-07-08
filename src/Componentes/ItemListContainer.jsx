ef67240695bd901d531e870086c6bf4cb58ccad3
Ddavid8576 <129808256+Ddavid8576@users.noreply.github.com>
2024-07-08 09:58:09 -0400

    Update ItemListContainer.jsx

--- src/Componentes/ItemListContainer.jsx
+++ src/Componentes/ItemListContainer.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const ItemListContainer = ({ saludo }) => {
+const ItemListContainer = ({ greeting }) => {
   return (
     <div style={{ display: 'flex', justifyContent: 'center'}} className="container mt-5">
-      <h2>{saludo}</h2>
+      <h2>{greeting}</h2>
     </div>
   );
 };
