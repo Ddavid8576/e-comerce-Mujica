df971a029edcc59ca9bff2b0fb838812e92dae25
Denynson Mujica <denynsonmujica@gmail.com>
2024-07-08 19:04:20 -0400

    cambio de nombre carpeta

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React from 'react';
-import NavBar from './Componentes/NavBar';
-import ItemListContainer from './Componentes/ItemListContainer';
+import NavBar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
